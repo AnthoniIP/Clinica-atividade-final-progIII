bd02f58ba925f11a3e52adc4fa27db17d27bc4f6
Anthoni Ipiranga <anthoni.ipiranga@gmail.com>
2022-02-10 22:57:08 -0300

    First commit

--- src/Main.java
+++ src/Main.java
@@ -27,34 +27,28 @@ public class Main {
         select = Integer.parseInt(JOptionPane.showInputDialog("ESCOLHA A ESPECIALIDADE: \n(1) - PEDIATRIA; \n(2) - ORTOPEDIA; \n(3) - CLÍNICO GERAL; \n(4) - NEUROLOGISTA:"));
 
 
-
-
-
-        switch (select){
-            case 1:
-                JOptionPane.showMessageDialog(null,"\nNome: " + pac.getNome()+"\nTelefone: " + pac.getTelefone()+"\nE-mail: " + pac.getEmail()+"\nAniversário: " + pac.getDatanasc()+"\nEndereço: " + pac.getEndereco()+"\nSexo: " + pac.getSexo());
-                Pediatra ped = new Pediatra("\nDr. Paulo","Manhã","Segunda-Feira");
-                JOptionPane.showMessageDialog(null,"\nInformações da consulta: "+ped.getDados());
-                break;
-            case 2:
-                JOptionPane.showMessageDialog(null,"\nNome: " + pac.getNome()+"\nTelefone: " + pac.getTelefone()+"\nE-mail: " + pac.getEmail()+"\nAniversário: " + pac.getDatanasc()+"\nEndereço: " + pac.getEndereco()+"\nSexo: " + pac.getSexo());
-                Ortopedista ort = new Ortopedista("Dr. Sandro","Tarde","Terça-Feira");
-                JOptionPane.showMessageDialog(null,"\nInformações da consulta: "+ort.getDados());
-                break;
-            case 3:
-                JOptionPane.showMessageDialog(null,"\nNome: " + pac.getNome()+"\nTelefone: " + pac.getTelefone()+"\nE-mail: " + pac.getEmail()+"\nAniversário: " + pac.getDatanasc()+"\nEndereço: " + pac.getEndereco()+"\nSexo: " + pac.getSexo());
-                Clinico clin = new Clinico("Dr. Carlos","Noite","Quarta-Feira");
-                JOptionPane.showMessageDialog(null,"\nInformações da consulta: "+clin.getDados());
-                break;
-            case 4:
-                JOptionPane.showMessageDialog(null,"\nNome: " + pac.getNome()+"\nTelefone: " + pac.getTelefone()+"\nE-mail: " + pac.getEmail()+"\nAniversário: " + pac.getDatanasc()+"\nEndereço: " + pac.getEndereco()+"\nSexo: " + pac.getSexo());
-                Neurologista neuro = new Neurologista("Dr. Rafael","Noite","Quinta-Feira");
-                JOptionPane.showMessageDialog(null,"\nInformações da consulta: "+neuro.getDados());
-                break;
-
-            default:
-                out.printf("OPERAÇÃO INVÁLIDA");
-
+        switch (select) {
+            case 1 -> {
+                JOptionPane.showMessageDialog(null, "\nNome: " + pac.getNome() + "\nTelefone: " + pac.getTelefone() + "\nE-mail: " + pac.getEmail() + "\nAniversário: " + pac.getDatanasc() + "\nEndereço: " + pac.getEndereco() + "\nSexo: " + pac.getSexo());
+                Pediatra ped = new Pediatra("\nDr. Paulo", "Manhã", "Segunda-Feira");
+                JOptionPane.showMessageDialog(null, "\nInformações da consulta: " + ped.getDados());
+            }
+            case 2 -> {
+                JOptionPane.showMessageDialog(null, "\nNome: " + pac.getNome() + "\nTelefone: " + pac.getTelefone() + "\nE-mail: " + pac.getEmail() + "\nAniversário: " + pac.getDatanasc() + "\nEndereço: " + pac.getEndereco() + "\nSexo: " + pac.getSexo());
+                Ortopedista ort = new Ortopedista("Dr. Sandro", "Tarde", "Terça-Feira");
+                JOptionPane.showMessageDialog(null, "\nInformações da consulta: " + ort.getDados());
+            }
+            case 3 -> {
+                JOptionPane.showMessageDialog(null, "\nNome: " + pac.getNome() + "\nTelefone: " + pac.getTelefone() + "\nE-mail: " + pac.getEmail() + "\nAniversário: " + pac.getDatanasc() + "\nEndereço: " + pac.getEndereco() + "\nSexo: " + pac.getSexo());
+                Clinico clin = new Clinico("Dr. Carlos", "Noite", "Quarta-Feira");
+                JOptionPane.showMessageDialog(null, "\nInformações da consulta: " + clin.getDados());
+            }
+            case 4 -> {
+                JOptionPane.showMessageDialog(null, "\nNome: " + pac.getNome() + "\nTelefone: " + pac.getTelefone() + "\nE-mail: " + pac.getEmail() + "\nAniversário: " + pac.getDatanasc() + "\nEndereço: " + pac.getEndereco() + "\nSexo: " + pac.getSexo());
+                Neurologista neuro = new Neurologista("Dr. Rafael", "Noite", "Quinta-Feira");
+                JOptionPane.showMessageDialog(null, "\nInformações da consulta: " + neuro.getDados());
+            }
+            default -> out.print("OPERAÇÃO INVÁLIDA");
         }
 
     }
